38a18cd723382d43bb3e0fc49480f3a832f7f3dd
Mohashan <msh200x@gmail.com>
2021-12-21 09:13:12 +0330

    Size and min and max value of types

--- Code/Chapter02/Vocabulary/Program.cs
+++ Code/Chapter02/Vocabulary/Program.cs
@@ -1,25 +1,4 @@
-﻿using System.Linq;
-using System.Reflection;
-
-Assembly? assembly = Assembly.GetEntryAssembly();
-if (assembly == null) return;
-
-// loop through the assemblies that this app references
-foreach (AssemblyName name in assembly.GetReferencedAssemblies())
-{
-    // load the assembly so we can read its details
-    Assembly a = Assembly.Load(name);
-    // declare a variable to count the number of methods
-    int methodCount = 0;
-    // loop through all the types in the assembly
-    foreach (TypeInfo t in a.DefinedTypes)
-    {
-        // add up the counts of methods
-        methodCount += t.GetMethods().Count();
-    }
-    // output the count of types and their methods
-    Console.WriteLine(
-    "{0:N0} types with {1:N0} methods in {2} assembly.",
-    arg0: a.DefinedTypes.Count(),
-    arg1: methodCount, arg2: name.Name);
-}
+﻿Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}."); 
+Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}."); 
+Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
+Console.WriteLine($"decimal uses {sizeof(Int64)} bytes and can store numbers in the range {Int64.MinValue:N0} to {Int64.MaxValue:N0}.");
