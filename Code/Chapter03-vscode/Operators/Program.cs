169f1fdd983b6e3851552b943a71e5976b5069e3
Mohashan <msh200x@gmail.com>
2021-12-24 16:46:57 +0330

    Pattern matching with the switch statement

--- Code/Chapter03-vscode/Operators/Program.cs
+++ Code/Chapter03-vscode/Operators/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+
+string path = @"../Operators/";
+Write("Press R for read-only or W for writeable: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+Stream? s;
+if (key.Key == ConsoleKey.R)
+{
+    s = File.Open(
+    Path.Combine(path, "Program.cs"),
+    FileMode.OpenOrCreate,
+    FileAccess.Read);
+}
+else
+{
+    s = File.Open(
+    Path.Combine(path, "Program.cs"),
+    FileMode.OpenOrCreate,
+    FileAccess.Write);
+}
+string message;
+switch (s)
+{
+    case FileStream writeableFile when s.CanWrite:
+        message = "The stream is a file that I can write to.";
+        break;
+    case FileStream readOnlyFile:
+        message = "The stream is a read-only file.";
+        break;
+    case MemoryStream ms:
+        message = "The stream is a memory address.";
+        break;
+    default: // always evaluated last despite its current position
+        message = "The stream is some other type.";
+        break;
+    case null:
+        message = "The stream is null.";
+        break;
+}
+WriteLine(message);
+
+
