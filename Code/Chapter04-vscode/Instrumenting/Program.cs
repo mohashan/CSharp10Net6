35616e37fef6427f77478df232756a50a77bb060
Mohashan <msh200x@gmail.com>
2022-01-06 22:54:42 +0330

    Add Debug and Trace log

---
+++ Code/Chapter04-vscode/Instrumenting/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using System.Diagnostics;
+// See https://aka.ms/new-console-template for more information
+
+Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt"))));
+Trace.AutoFlush = true;
+
+Debug.WriteLine("Debug says!");
+Trace.WriteLine("Trace says!");
