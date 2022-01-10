268cc61bb887bfb0e8a1397a9d3dcf8e245baf18
Mohashan <msh200x@gmail.com>
2022-01-10 22:23:09 +0330

    Pattern matching in CSharp 8

--- Code/Chapter05-vscode/PeopleApp/Program.cs
+++ Code/Chapter05-vscode/PeopleApp/Program.cs
@@ -14,9 +14,16 @@ foreach (object passenger in passengers)
 {
     decimal flightCost = passenger switch
     {
-        FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
-        FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
-        FirstClassPassenger _ => 2000M,
+        // FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
+        // FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
+        // FirstClassPassenger _ => 2000M,
+
+        FirstClassPassenger p => p.AirMiles switch
+        {
+            > 35000 => 1500M,
+            > 15000 => 1750M,
+            _ => 2000M
+        },
         BusinessClassPassenger _ => 1000M,
         CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
         CoachClassPassenger _ => 650M,
