ec7f7d60de7bc16ced974ff4c9983e623320ae3f
Mohashan <msh200x@gmail.com>
2022-01-10 22:59:53 +0330

    Change Properties of Records

--- Code/Chapter05-vscode/PeopleApp/Program.cs
+++ Code/Chapter05-vscode/PeopleApp/Program.cs
@@ -2,32 +2,16 @@
 using System;
 using static System.Console;
 
-object[] passengers = {
- new FirstClassPassenger { AirMiles = 1_419 },
- new FirstClassPassenger { AirMiles = 16_562 },
- new BusinessClassPassenger(),
- new CoachClassPassenger { CarryOnKG = 25.7 },
- new CoachClassPassenger { CarryOnKG = 0 },
+ImmutableVehicle car = new()
+{
+    Brand = "Mazda MX-5 RF",
+    Color = "Soul Red Crystal Metallic",
+    Wheels = 4
 };
 
-foreach (object passenger in passengers)
-{
-    decimal flightCost = passenger switch
-    {
-        // FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
-        // FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
-        // FirstClassPassenger _ => 2000M,
+ImmutableVehicle repaintedCar = car
+ with
+{ Color = "Polymetal Grey Metallic" };
 
-        FirstClassPassenger p => p.AirMiles switch
-        {
-            > 35000 => 1500M,
-            > 15000 => 1750M,
-            _ => 2000M
-        },
-        BusinessClassPassenger _ => 1000M,
-        CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
-        CoachClassPassenger _ => 650M,
-        _ => 800M
-    };
-    WriteLine($"Flight costs {flightCost:C} for {passenger}");
-}
+WriteLine($"Original car color was {car.Color}.");
+WriteLine($"New car color is {repaintedCar.Color}.");
