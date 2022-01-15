20f834ba9e4b93479bee2802efc12ec9ac545dfc
Mohashan <msh200x@gmail.com>
2022-01-15 14:26:55 +0330

    Add Static and instace method

---
+++ Code/Chapter06-vscode/PacktLibrary/Person.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+namespace Packt.Shared;
+public class Person : object
+{
+    // fields
+    public string? Name; // ? allows null
+    public DateTime DateOfBirth;
+    public List<Person> Children = new(); // C# 9 or later
+                                          // methods
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+
+    public static Person Procreate(Person p1, Person p2)
+    {
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name}"
+        };
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+        return baby;
+
+    }
+    public Person ProcreateWith(Person partner)
+    {
+        return Procreate(this, partner);
+    }
+}
---
+++ Code/Chapter06-vscode/PeopleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Packt.Shared;
+using static System.Console; 
+
+Person harry = new() { Name = "Harry" }; 
+Person mary = new() { Name = "Mary" }; 
+Person jill = new() { Name = "Jill" };
+
+Person baby1 = mary.ProcreateWith(harry); 
+baby1.Name = "Gary";
+
+Person baby2 = Person.Procreate(harry, jill);
+
+WriteLine($"{harry.Name} has {harry.Children.Count} children."); 
+WriteLine($"{mary.Name} has {mary.Children.Count} children."); 
+WriteLine($"{jill.Name} has {jill.Children.Count} children."); 
+WriteLine(
+ format: "{0}'s first child is named \"{1}\".",
+ arg0: harry.Name,
+ arg1: harry.Children[0].Name);
